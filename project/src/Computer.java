a3f5eac550901f222dffd3f7ca9eba3045166746
Isheanopa M <imay.pubdev@gmail.com>
2022-05-26 17:38:02 +0200

    renamed 'Main' class to 'ComputerSwingApp'

--- project/src/Computer.java
+++ project/src/Computer.java
@@ -1,3 +1,29 @@
 public class Computer {
 
+   String m_ip_address;
+   String m_value;
+
+   public Computer() {
+      this.m_ip_address = "";
+      this.m_value = "";
+   }
+
+   public Computer(String ip_address, String value) {
+      this.m_ip_address = ip_address;
+      this.m_value = value;
+   }
+
+   public String GetValue() {
+      return this.m_value;
+   }
+
+   public String GetIPAddress() {
+      return this.m_ip_address;
+   }
+
+   @Override
+   public String toString() {
+      return "Address:\t" + GetIPAddress() + "\n"
+            + "Value:\t" + GetValue() + "\n";
+   }
 }
--- project/src/ComputerSwingApp.java
+++ project/src/ComputerSwingApp.java
@@ -5,7 +5,7 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.util.ArrayList;
 
-public class Main {
+public class ComputerSwingApp {
 
    public static void main(String[] args) {
 
